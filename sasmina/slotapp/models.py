56b1ac3e0074f0fc1bd03d830f21aa857153cc48
sasmina222-boop <sasmina222@gmail.com>
2025-11-28 21:18:54 +0530

    success

--- sasmina/slotapp/models.py
+++ sasmina/slotapp/models.py
@@ -1,47 +1,22 @@
-# Ex03 Time Table
-## Date: 28/11/2025
-## Reg. no: 25019029
+from django.db import models
+from django.contrib import admin
 
-## AIM
-To write a html webpage page to display your slot timetable.
-
-## ALGORITHM
-### STEP 1
-Create a Django-admin Interface.
-
-### STEP 2
-Create a static folder and inert HTML code.
-
-### STEP 3
-Create a simple table using ```<table>``` tag in html.
-
-### STEP 4
-Add header row using ```<th>``` tag.
-
-### STEP 5
-Add your timetable using ```<td>``` tag.
-
-### STEP 6
-Execute the program using runserver command.
-
-## PROGRAM
-~~~
 <html>
     <head>
         <title>Time Table</title>
         <style>
             table, th, td {
-                border: 1px solid black;
+                border: 2px solid black;
              }
         </style>
     </head>
     <body>
 
-        <h3 colspan="4" align="center">SLOT TIME TABLE - SASMINA S(25019029)</h3>
+        <h3 colspan="4" align="center">SLOT TIME TABLE - SASMINA (25019029)</h3>
         <table id="1" border="50%" width="600" bgcolor="cyan">
             <tr class="header-row">
                 <th bgcolor="yellow">Day/Time</th>
-                <th bgcolor="yellow">Monday</th>
+                <th bgcolor="yellow">Monday</th> 
                 <th bgcolor="yellow">Tuesday</th>
                 <th bgcolor="yellow">Wednesday</th>
                 <th bgcolor="yellow">Thursday</th>
@@ -122,10 +97,3 @@ Execute the program using runserver command.
         </table>
     </body>
 </html>
-~~~
-
-## OUTPUT
-![alt text](image-1.png)
-
-## RESULT
-The program for creating slot timetable using basic HTML tags is executed successfully.
